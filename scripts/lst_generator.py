428bb34a5a59fb8a8108917583589b93a9b338b8
Federico Costa <federico.costa@veue02.tsc.upc.edu>
2022-08-13 19:54:53 +0200

    added lst file generator

---
+++ scripts/lst_generator.py
@@ -0,0 +1,33 @@
+import os
+
+# were to look for files to make the feature extraction
+load_path = "/home/usuaris/veu/federico.costa/datasets/voxceleb1/test"
+
+# output file path
+dump_path = "/home/usuaris/veu/federico.costa/git_repositories/DoubleAttentionSpeakerVerification/files_directories/feature_extractor/feature_extractor_paths.lst"
+
+valid_formats = [".wav"]
+
+number_of_files = 0
+
+with open(dump_path, 'w') as f:
+
+    print(f"Searching {valid_formats} files in {load_path}")
+    print("-"*50)
+
+    for (dir_path, dir_names, file_names) in os.walk(load_path):
+
+        print(f"Searching in {dir_path}")
+
+        for file_name in file_names:
+            if file_name[-4:] in valid_formats:
+                path_to_write = f"{dir_path}/{file_name}"
+                f.write(path_to_write)
+                f.write('\n')
+                number_of_files = number_of_files + 1
+
+    print("-"*50)
+    print(f"{number_of_files} files paths dumped in {dump_path}")
+
+    
+
