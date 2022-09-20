3520392625b2a2f5cc8e5a755c0f64ba9f28863a
Federico Costa <federico.costa@veue02.tsc.upc.edu>
2022-09-20 16:12:33 +0200

    log name

--- scripts/train.py
+++ scripts/train.py
@@ -288,7 +288,7 @@ if __name__=="__main__":
     parser.add_argument('--data_mode', type = str, default = 'normal', choices=['normal','window'])
     parser.add_argument('--valid_clients', type = str, default='/home/usuaris/veu/federico.costa/git_repositories/DoubleAttentionSpeakerVerification/scripts/labels/clients.ndx')
     parser.add_argument('--valid_impostors', type = str, default='/home/usuaris/veu/federico.costa/git_repositories/DoubleAttentionSpeakerVerification/scripts/labels/impostors.ndx')
-    parser.add_argument('--out_dir', type=str, default='./models/model1', help='directory where data is saved')
+    parser.add_argument('--out_dir', type=str, default='./models/model2', help='directory where data is saved')
     parser.add_argument('--model_name', type=str, default='CNN', help='Model associated to the model builded')
     parser.add_argument('--front_end', type=str, default='VGG4L', choices = ['VGG3L','VGG4L'], help='Kind of Front-end Used')
     
